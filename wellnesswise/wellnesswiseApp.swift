cdc623b4b69d1a62c25c374fbbe38b1a5894f762
zafar ali <zafaralyjamali722@gmail.com>
2024-10-09 18:04:50 +0500

    Initial Commit

---
+++ wellnesswise/wellnesswiseApp.swift
@@ -0,0 +1,17 @@
+//
+//  wellnesswiseApp.swift
+//  wellnesswise
+//
+//  Created by Zafar Ali on 09/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct wellnesswiseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ wellnesswiseTests/wellnesswiseTests.swift
@@ -0,0 +1,17 @@
+//
+//  wellnesswiseTests.swift
+//  wellnesswiseTests
+//
+//  Created by Zafar Ali on 09/10/2024.
+//
+
+import Testing
+@testable import wellnesswise
+
+struct wellnesswiseTests {
+
+    @Test func testExample() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
