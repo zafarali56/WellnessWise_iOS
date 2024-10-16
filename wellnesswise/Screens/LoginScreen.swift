0bbd4c3fbdea3cd58e8b2817aabde564e8a71886
zafar ali <zafaralyjamali722@gmail.com>
2024-10-16 11:28:46 +0500

    Login screen adding elements also adding some UI assests.

--- wellnesswise/Screens/LoginScreen.swift
+++ wellnesswise/Screens/LoginScreen.swift
@@ -8,8 +8,35 @@
 import SwiftUI
 
 struct LoginScreen: View {
+    @State private var email: String = ""
+    @State private var password: String = ""
     var body: some View {
-        Text("Login screen")
+        VStack {
+            Spacer(minLength: 100)
+            Image(.whitetheme)
+                .frame(width: 200, height: 200, alignment: .center)
+           
+           
+            Form {
+                Text("Login")
+                    .bold()
+                    .font(.title)
+                TextField(
+                    "Email",
+                    text:$email
+                )
+                SecureField (
+                    "Password",
+                    text:  $password)
+            }
+            Button {
+                
+            }
+            label: {
+                Text("Login")
+            }
+            
+        }
     }
 }
 
