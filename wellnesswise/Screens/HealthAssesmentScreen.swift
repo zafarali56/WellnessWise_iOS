90670585dc2119db333aa3b90e99965dda303383
zafar ali <zafaralyjamali722@gmail.com>
2024-11-01 23:35:11 +0500

    Added pickers in health assesment and health assesment viewModel

--- wellnesswise/Screens/HealthAssesmentScreen.swift
+++ wellnesswise/Screens/HealthAssesmentScreen.swift
@@ -8,8 +8,47 @@
 import SwiftUI
 
 struct HealthAssesmentScreen: View {
+	@StateObject private var viewModel = HealthAssesmentViewModel()
     var body: some View {
-        Text("Hello, World!")
+		VStack{
+			Text("Health Assessment").font(.title2)
+			Form {
+				Section(header: Text("Medical history") )
+				{
+					Picker(selection: $viewModel.familyDiabetes, label: Text("Family history of Diabetes"))
+					{
+						Text("Yes").tag ("yes")
+						Text("No").tag ("no")
+					}
+					
+					Picker(
+						selection: $viewModel.heartDisease, label: Text("Family history of heart disease"))
+					{
+						Text ("Yes").tag("yes")
+						Text("No").tag ("no")
+					}
+					
+					Picker (
+						selection : $viewModel.familyHistoryCancer , label: Text("Family history of cancer")) {
+							Text("Yes").tag("yes")
+							Text("No").tag("no")
+						}
+					
+					Picker (selection: $viewModel.previousSurgeries, label: Text ("Preview surgeries"))
+					{
+						Text("Yes").tag ("yes")
+						Text("No").tag ("no")
+					}
+					
+					Picker (selection: $viewModel.choricDiseases, label: Text("Chronic diseases"))
+					{
+						Text("Yes").tag("yes")
+						Text("No").tag("no")
+					}
+				}
+				
+			}
+		}
     }
 }
 
--- wellnesswise/ViewModel/HealthAssesmentViewModel.swift
+++ wellnesswise/ViewModel/HealthAssesmentViewModel.swift
@@ -6,3 +6,30 @@
 //
 
 import Foundation
+
+
+class HealthAssesmentViewModel : ObservableObject {
+	
+	//Medical History
+	@Published var familyDiabetes : String = ""
+	@Published var heartDisease : String = ""
+	@Published var familyHistoryCancer : String = ""
+	@Published var previousSurgeries : String = ""
+	@Published var choricDiseases : String = ""
+	
+	//LifeStyle habits
+	@Published var smoke : String = ""
+	@Published var alcoholConsumptionLevel : String = ""
+	@Published var physicalActivity : String = ""
+	@Published var dietQuality : String = ""
+	@Published var sleepHour : String = ""
+	
+	//Envirnomental Factors
+	@Published var airQuality : String = ""
+	@Published var pollutantExposure : String = ""
+	
+	//Additional Information
+	@Published var stressLevel : String = ""
+	@Published var AccessToHealthCare : String = ""
+	
+}
