49dabf09bc4cf85263ece15ee4b1f9ef322a947c
zafar ali <zafaralyjamali722@gmail.com>
2024-11-04 23:43:46 +0500

    Added submitAssesment func in the viewModel of healthAssesmentScreen

--- wellnesswise/ViewModel/HealthAssesmentViewModel.swift
+++ wellnesswise/ViewModel/HealthAssesmentViewModel.swift
@@ -6,26 +6,28 @@
 //
 
 import Foundation
-
+import FirebaseAuth
+import FirebaseFirestore
 
 class HealthAssesmentViewModel : ObservableObject {
 	
 	//Medical History
-	@Published var familyDiabetes : String = ""
-	@Published var heartDisease : String = ""
-	@Published var familyHistoryCancer : String = ""
-	@Published var previousSurgeries : String = ""
-	@Published var choricDiseases : String = ""
+	@Published var familyDiabetes : String = "no"
+	@Published var heartDisease : String = "no"
+	@Published var familyHistoryCancer : String = "no"
+	@Published var previousSurgeries : String = "no"
+	@Published var chronicDiseases : String = "no"
 	
 	//LifeStyle habits
-	@Published var smoke : String = ""
+	@Published var smoke : String = "no"
 	@Published var selectedAlcholoLevel = "None"
 	let AlcholoConsumptionLevel = ["None", "Light", "Moderate", "Heavy", "Very Heavy"]
-	@Published var selectedPhysicalActivityLevel = "Active"
+	
+	@Published var selectedPhysicalActivityLevel = "Moderate"
 	let physicalActivityLevel = ["Sedentary", "Light", "Moderate", "Active", "Very Active"]
 	
 	
-	@Published var selctedDietQuality : String = "Good"
+	@Published var selectedDietQuality : String = "Good"
 	let dietQualityLevel = ["Poor", "Fair", "Good", "Very Good", "Excellent"]
 	
 	@Published var sleepHour : String = ""
@@ -38,4 +40,52 @@ class HealthAssesmentViewModel : ObservableObject {
 	@Published var stressLevel : String = ""
 	@Published var AccessToHealthCare : String = ""
 	
+	
+	@Published var isLoading = false
+	@Published var errorMessage = ""
+	@Published var isAssesmentCompleted = false
+	
+	
+	func submitAssesment () {
+		guard let userId = Auth.auth().currentUser?.uid else {
+			errorMessage = "User not authenticated"
+			return
+		}
+		
+		var isLoading = true
+		
+		
+		let assessmentData: [String: Any] = [
+			"medicalHistory": [
+				"familyDiabetes" : familyDiabetes,
+				"heartDisease" : heartDisease,
+				"familyHistoryCancer" : familyHistoryCancer,
+				"previousSurgeries" : previousSurgeries,
+				"chronicDiseases" : chronicDiseases
+			],
+			
+			"lifeStyleHabits" : [
+				"smoke" : smoke,
+				"alcoholLevel" : selectedAlcholoLevel,
+				"physicalActivityLevel" : physicalActivityLevel,
+				"dietQuality" : selectedDietQuality
+			],
+			"timeStamp": FieldValue.serverTimestamp()
+		]
+		Firestore.firestore().collection("users")
+			.document(userId)
+			.collection("assesments")
+			.addDocument(data: assessmentData) {[weak self] error in
+				self?.isLoading = false
+				
+				if let error = error{
+					self?.errorMessage = error.localizedDescription
+				}
+				else {
+					self?.isAssesmentCompleted = true
+				}
+			}
+	}
+	
 }
+
