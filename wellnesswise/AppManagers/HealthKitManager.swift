afc2de3ef47414502f21c2ecbab647e60f9f629c
zafar ali <zafaralyjamali722@gmail.com>
2024-12-27 23:41:08 +0500

    Integrating health Kit to the App for real time Data Sync

---
+++ wellnesswise/AppManagers/HealthKitManager.swift
@@ -0,0 +1,15 @@
+//
+//  HealthKitManager.swift
+//  wellnesswise
+//
+//  Created by Zafar Ali on 27/12/2024.
+//
+
+import Foundation
+import HealthKit
+class HealthKitManager {
+	//Instance of healthKitStore
+	let healthStore = HKHealthStore()
+	
+	
+}
