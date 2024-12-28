dd1eb03260a901950bfd717e4514cc1f47763dc9
zafar ali <zafaralyjamali722@gmail.com>
2024-12-28 20:34:23 +0500

    Added access to healthKit manager class for accesing the fileds from ihealth APP, heart rate bp and Blood sugar-/

--- wellnesswise/AppManagers/HealthKitManager.swift
+++ wellnesswise/AppManagers/HealthKitManager.swift
@@ -10,6 +10,41 @@ import HealthKit
 class HealthKitManager {
 	//Instance of healthKitStore
 	let healthStore = HKHealthStore()
-	
-	
+	//Requesting Authorization for HeartRate/BP/BS
+	func requestAuthorization (completion: @escaping (Bool, Error?) -> Void ){
+		guard let heartRateType = HKObjectType.quantityType(
+			forIdentifier: .heartRate),
+			  let systolicType = HKObjectType.quantityType(
+				forIdentifier: .bloodPressureSystolic
+			  ),
+			  let diastolicType = HKObjectType.quantityType(
+				forIdentifier: .bloodPressureSystolic
+			  ),
+			  let bloodGlucoseType = HKObjectType.quantityType(
+				forIdentifier: .bloodGlucose
+			  )
+		else {
+			completion(false, nil)
+			return}
+		//Adding these types to set a read permission
+		let readTypes : Set<HKObjectType> = [
+			heartRateType,
+			systolicType,
+			diastolicType,
+			bloodGlucoseType
+		]
+		
+		healthStore.requestAuthorization(toShare: nil, read: readTypes) {
+			success,
+			error in completion(success,error)
+		}
+	}
+}
+
+
+extension HealthKitManager {
+	func retriveLatestHearRate (completion: @escaping (Double?, Error?) -> Void ){guard let heartRateType = HKQuantityType.quantityType(forIdentifier: .heartRate)else{completion(nil, nil)
+		return
+		}
+	}
 }
