3a5b42caff3d5e94bf610c94a73cf010bc3304a1
zafarali <zafaralyjamali722@gmail.com>
2025-03-01 22:59:02 +0500

    *new

---
+++ wellnesswise/Screens/DataVisualizationViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  DataVisualizationViewModel.swift
+//  wellnesswise
+//
+//  Created by Zafar ‎ on 01/03/2025.
+//
+
+import SwiftUI
+
+struct DataVisualizationViewModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DataVisualizationViewModel()
+}
---
+++ wellnesswise/ViewModel/VisualizationViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  VisualizationViewModel.swift
+//  wellnesswise
+//
+//  Created by Zafar ‎ on 01/03/2025.
+//
+
+import Foundation
